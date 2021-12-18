25b149c8b67de9e7448367434e70e7a05d6f637f
Rohit Venkat <rohitrvenkat@gmail.com>
2021-12-18 09:44:51 -0600

    Continue building shiny app

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -1,8 +1,14 @@
+library(tidyverse)
 library(shiny)
 library(fresh)
-library(tidyverse)
-library(shinyDataFilter)
+library(shinyjs)
+library(leaflet)
+library(leaflet.extras)
+library(sf)
 library(DT)
+library(shinyDataFilter)
+
+census_data <- readRDS("data/census_data.rds")
 
 hmda_data <- readRDS("data/HMDA_2020_WA_data.rds") %>%
   select(activity_year,
@@ -48,7 +54,6 @@ attr(hmda_data$loan_term, "label") <- "The number of months after which the lega
 attr(hmda_data$loan_type, "label") <- "The type of covered loan or application"
 attr(hmda_data$purchaser_type, "label") <- "Type of entity purchasing a covered loan from the institution"
 
-
 colnames(hmda_data) <- c("Activity Year",
                          "Lender Name",
                          "LEI",
@@ -65,3 +70,62 @@ colnames(hmda_data) <- c("Activity Year",
                          "Denial Reason #1",
                          "Denial Reason #2",
                          "Denial Reason #3")
+
+draw_base_map <- function() {
+  leaflet(
+    options = leafletOptions(minZoom = 6, maxZoom = 14)
+  ) %>% 
+    addProviderTiles("CartoDB.Positron") %>% 
+    setView(lng = -120.7401, lat = 47.7511, zoom = 6) %>% 
+    addResetMapButton() %>%
+    addEasyButton(
+      easyButton(icon = htmltools::HTML("<i class='fas fa-filter'></i>"),
+                 title = "Filter",
+                 onClick = JS("function(btn, map) {
+                              Shiny.onInputChange('filterButton', '1');
+                              Shiny.onInputChange('filterButton', '2'); 
+                              }")
+      )
+    )
+}
+
+pal <- colorNumeric(palette = "viridis", domain = NULL)
+
+update_shapes <- function(mymap, my_data) {
+  leafletProxy(mymap, data = my_data) %>% 
+    clearShapes() %>%
+    addPolygons(
+      data = my_data$geometry,
+      stroke = TRUE,
+      weight = 1,
+      opacity = 1,
+      color = "white",
+      fillOpacity = 0.6,
+      fillColor = pal(my_data$population),
+      # label = ~ lapply(tooltip, HTML),
+      highlight = highlightOptions(
+        weight = 3,
+        fillOpacity = 0.8,
+        color = "#666",
+        bringToFront = FALSE)
+    )
+}
+
+draw_map_legend <- function(mymap, df) {
+  leafletProxy(mymap, data = df) %>%
+    clearControls() %>%
+    addLegend(
+      "bottomleft",
+      pal = pal, 
+      values = ~ population,
+      title = ~ "Population",
+      opacity = 1
+    )
+}
+
+check_zoom <- function(zoom) {
+  case_when(
+    zoom <= 7 ~ "county",
+    TRUE ~ "tract"
+  )
+}
--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,63 +1,107 @@
+library(tidyverse)
 library(shiny)
 library(fresh)
-library(tidyverse)
-library(shinyDataFilter)
+library(shinyjs)
+library(leaflet)
+library(leaflet.extras)
+library(sf)
 library(DT)
+library(shinyDataFilter)
 
 shinyServer(function(input, output) {
-    
-    filtered_data_1 <- callModule(
-      shiny_data_filter,
-      "data_filter_1",
-      data = hmda_data,
-      verbose = FALSE)
-    
-    filtered_data_2 <- callModule(
-      shiny_data_filter,
-      "data_filter_2",
-      data = hmda_data,
-      verbose = FALSE)
+  
+  observeEvent(input$filterButton, {
+    toggle('filterPanel')
+  })
+  
+  output$mymap <- renderLeaflet({
+    draw_base_map()
+  })
+  
+  rv <- reactiveValues(scope = "county")
 
-    filtered_data_3 <- callModule(
-      shiny_data_filter,
-      "data_filter_3",
-      data = hmda_data,
-      verbose = FALSE)
-    
-    output$plot_1 <- renderPlot({
-      filtered_data_1() %>%
-        ggplot(aes(x = `Loan Amount`)) +
-        geom_histogram()
-    })
-    
-    output$data_table <- renderDT(
-      server = TRUE, {
-      datatable(
-        filtered_data_3(),
-        style = 'bootstrap',
-        rownames = FALSE,
-        selection = "none",
-        extensions = c("Buttons"),
-        options = list(
-          pageLength = 10,
-          lengthMenu = c(10, 25, 50, 100),
-          autoWidth = TRUE,
-          scrollX = TRUE,
-          dom = 'lrtip',
-          buttons = list(
-            list(
-              extend = 'collection',
-              buttons = c('columnsToggle'),
-              text = 'Columns'
-            )
+  observeEvent(input$mymap_zoom, {
+    rv$scope <- check_zoom(input$mymap_zoom)
+  })
+
+  my_sf <- reactive({
+    census_data %>%
+      filter(scope == rv$scope)
+  })
+
+  observeEvent(rv$scope, {
+    update_shapes("mymap", my_sf())
+  })
+
+  observeEvent(my_sf(), {
+    draw_map_legend("mymap", my_sf())
+  })
+  
+  filtered_data_1 <- callModule(
+    shiny_data_filter,
+    "data_filter_1",
+    data = hmda_data,
+    verbose = FALSE)
+  
+  filtered_data_2 <- callModule(
+    shiny_data_filter,
+    "data_filter_2",
+    data = hmda_data,
+    verbose = FALSE)
+
+  filtered_data_3 <- callModule(
+    shiny_data_filter,
+    "data_filter_3",
+    data = hmda_data,
+    verbose = FALSE)
+  
+  filtered_data_4 <- callModule(
+    shiny_data_filter,
+    "data_filter_4",
+    data = hmda_data,
+    verbose = FALSE)
+  
+  output$plot_1 <- renderPlot({
+    filtered_data_1() %>%
+      ggplot(aes(x = `Loan Amount`)) +
+      geom_histogram()
+  })
+  
+  output$plot_2 <- renderPlot({
+    ggplot() +
+      geom_boxplot(data = filtered_data_1(), aes(x = "Group 1", y = `Loan Amount`, color = "Group 1")) +
+      geom_boxplot(data = filtered_data_2(), aes(x = "Group 2", y = `Loan Amount`, color = "Group 2")) +
+      scale_y_log10()
+  })
+
+  output$data_table <- renderDT(
+    server = TRUE, {
+    datatable(
+      filtered_data_3(),
+      style = 'bootstrap',
+      rownames = FALSE,
+      selection = "none",
+      extensions = c("Buttons"),
+      options = list(
+        pageLength = 10,
+        lengthMenu = c(10, 25, 50, 100),
+        autoWidth = TRUE,
+        scrollX = TRUE,
+        dom = 'lrtip',
+        buttons = list(
+          list(
+            extend = 'collection',
+            buttons = c('columnsToggle'),
+            text = 'Columns'
           )
         )
       )
-    })
-    
-    output$download <- downloadHandler(
-      filename = 'download.csv', 
-      content = function(file) {
-        write.csv(filtered_data_3(), file, row.names = FALSE)
-    })
+    )
+  })
+  
+  output$download <- downloadHandler(
+    filename = 'download.csv', 
+    content = function(file) {
+      write.csv(filtered_data_3(), file, row.names = FALSE)
+  })
 })
